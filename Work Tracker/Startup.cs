db170e5e3c9b849d5be03f8cb84ac5c1aee02902
kolatts <kolatts@gmail.com>
2015-02-12 16:27:59 -0500

    Revert "Revert "asdasd""
    This reverts commit c86c8f7c9a78f0c9dd86f0e83878384c5616e429.

---
+++ Work Tracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Work_Tracker.Startup))]
+namespace Work_Tracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
